ab9b52603bb3bf036e2a1120e1cd4d090b3b1f74
Alfian Losari <alfianlosari@gmail.com>
2020-02-21 18:08:54 +0700

    Add Dispatch Queue background when generating code

---
+++ SwiftGenUIMac/View/Forms/ProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressView.swift
+//  SwiftGenUIMac
+//
+//  Created by Alfian Losari on 21/02/20.
+//  Copyright © 2020 Alfian Losari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressView()
+    }
+}
